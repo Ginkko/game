a2edc2fabacded822758b80d758913cb7c1152a9
Ginkko <Incubusalec@gmail.com>
2015-05-19 10:41:16 -0700

    Add move_north method. passes spec.

--- lib/Entity.rb
+++ lib/Entity.rb
@@ -1,3 +1,13 @@
 class Entity < ActiveRecord::Base
 
+  def damage
+    return 5
+  end
+
+  def move_north
+    location = self.location_y
+    location += 1
+    self.update(location_y: location)
+  end
+
 end
--- spec/entity_spec.rb
+++ spec/entity_spec.rb
@@ -2,10 +2,25 @@ require 'spec_helper'
 
 describe Entity do
 
-  describe '#spawn_player' do
+  describe '.create' do
     it 'creates a starting player with a name, health, level, and starting location of 1,1' do
       player = Entity.create(name: 'Dirge', level: 1, health: 100, location_x: 1, location_y: 1, pc?: true, alive?: true)
     expect(player.name).to eq('Dirge')
     end
   end
+
+  describe '#damage' do
+    it 'returns a damage roll for the entity' do
+      player = Entity.create(name: 'Dirge', level: 1, health: 100, location_x: 1, location_y: 1, pc?: true, alive?: true)
+      expect(player.damage).to eq(5)
+    end
+  end
+  describe '#move_north' do
+    it 'moves an entity one spot north' do
+      player = Entity.create(name: 'Dirge', level: 1, health: 100, location_x: 1, location_y: 1, pc?: true, alive?: true)
+      player.move_north
+      expect(player.location_y).to eq(2)
+    end
+  end
+
 end
